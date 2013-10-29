90329952795ac89416bb2f5ea80654a31ff08206
Han Jiang <han@apache.org>
2013-10-29 20:53:08 +0800

    update tool, supports multiple file

--- tools/nospace.sh
+++ tools/nospace.sh
@@ -2,8 +2,11 @@
 function usage() {
   echo "  Remove bogus whitespace from file"
   echo "  usage:"
-  echo "    $0 [file name]"
+  echo "    $0 [file names]"
   exit 1
 }
-(( $# == 1 )) || usage
-sed -i 's/　/  /g' $1
+(( $# >= 1 )) || usage
+for x in $*;
+do
+  sed -i 's/　/  /g' $x
+done
