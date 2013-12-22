76f11b1eb25f3c6d2db5f855728b7536c2ec0ab9
Han Jiang <han@apache.org>
2013-12-22 11:14:54 +0800

    resolve TODO: review words based on fail rate

--- tools/review.sh
+++ tools/review.sh
@@ -3,6 +3,9 @@
 # Currently, it make uses top 50 fails from 
 # the logfile
 
+# How many fallible words to review
+TOPN=50
+
 # LC_COLLATE:
 # "This variable determines the locale category for character collation. 
 #  It determines collation information for regular expressions and sorting, 
@@ -21,16 +24,23 @@ HEAD=`mktemp`
 HARD=`mktemp`
 
 # get full dicts 
-tail -q --lines=+2 data/dict/*.dat | sed '/^$/d' | sort -k2 > $FULL
+tail -q --lines=+2 data/dict/*.dat | \
+  sed '/^$/d'                      | \
+  sort -k2 > $FULL
 
 # get most fallible words
-tail -q --lines=+2 log/dict.im.dat | sort -nrk2 | head -50 | awk '{print $3}' | sort -k1 > $HEAD
+tail -q --lines=+2 log/dict.im.dat      | \
+  awk '{print $3, $2/$1}'               | \
+  sort -grk2                            | \
+  head -$TOPN                           | \
+  awk '{printf "%s [%0.2f]\n", $1, $2}' | \
+  sort -k1 > $HEAD
 
 # steal the dict head
 cat data/dict/*.dat | head -1 > $HARD
 
 # join and get a fake dictionary, containing most fallible words
-join -1 1 -2 2 $HEAD $FULL | awk '{t=$1;$1=$2;$2=t;}1' >> $HARD
+join -1 2 -2 1 $FULL $HEAD | awk '{t=$1;$1=$2;$2=t;print}' >> $HARD
 
 rm -f $HEAD $FULL
 
