52ef739fac90767105bfc0f25fd49b65d0cd5245
Han Jiang <han@apache.org>
2014-06-03 18:47:39 +0800

    add util to print out tests without chinese hints

---
+++ tools/hidehint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Used to practise kana/kanji->chinese
+
+export LC_COLLATE=C
+
+CUR=`readlink -f $0`
+DIR=`dirname $CUR`
+cd $DIR
+cd ..
+
+# by default we only use lesson files
+DICTS=`ls data/dict/lesson1[6-9].dat`
+DICTS=$DICTS' '`ls data/dict/lesson2*.dat`
+DICTS=$DICTS' '`ls data/dict/lesson3*.dat`
+
+(( $# >= 1)) && DICTS=$*
+
+tail -q --lines=+2 $DICTS  | \
+  awk '{
+    if (match($3, /^\[.*\]$/)) {
+      print $3;
+    } else {
+      print $2;
+    }
+  }' | tr -d '[]' | sed '/^$/d' | sort | uniq
